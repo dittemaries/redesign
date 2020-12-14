00b66bb4fc2b602c4d69f775c7da15dd5831e6d9
Kira Berthelsen <kira@MacBook-Pro-tilhrende-Kira.local>
2020-12-14 14:53:07 +0100

    commit3

---
+++ forside.js
@@ -0,0 +1,23 @@
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("sidenVises");
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+}
+
+function toggleMenu() {
+    console.log("toggleMenu")
+
+    document.querySelector("#menu").classList.toggle("hidden");
+
+    //    indeholder nogle af klasserne hidden
+    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        document.querySelector("#menuknap").textContent = "☰";
+    } else {
+        document.querySelector("#menuknap").textContent = "✕";
+
+    }
+
+}
